afc77601561ca116a5c8b00732c614d347384564
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-12-17 00:54:32 +0200

    add mobile styles

--- index.js
+++ index.js
@@ -1,36 +1,118 @@
 "use strict";
+
+const isMobile = document.documentElement.clientWidth < 768;
+const anchors = document.querySelectorAll(".nav-bar__nav-link--anchor");
+const acc = document.querySelector(".accordion");
+const categotiesBtns = document.querySelectorAll(".categories__link");
+const servicesLists = document.querySelectorAll(".services__category-item");
+const panel = document.querySelector(".panel");
+
 (function () {
   const header = document.querySelector(".header");
   const hero = document.querySelector(".hero");
   const foterLinks = document.querySelector(".footer__links-list");
   const senseComand = document.querySelector(".sense-comand");
-  console.log(foterLinks);
-  senseComand.style.width = foterLinks.offsetWidth + "px";
 
-  hero.style.height = `${window.innerHeight - header.offsetHeight}px`;
+  senseComand.style.width = foterLinks.offsetWidth + "px";
+  if (isMobile) {
+    acc.querySelector("span").textContent = "Перелік послуг";
+  }
+  hero.style.height = `${
+    document.documentElement.clientHeight - header.offsetHeight
+  }px`;
   window.addEventListener("resize", () => {
-    hero.style.height = `${window.innerHeight - header.offsetHeight}px`;
+    hero.style.height = `${
+      document.documentElement.clientHeight - header.offsetHeight
+    }px`;
   });
 })();
 
-const acc = document.querySelector(".accordion");
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-acc.addEventListener("click", function () {
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+acc.addEventListener("click", accordionHandler);
+
+function accordionHandler(handler) {
   /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-  this.classList.toggle("active");
+
+  acc.classList.toggle("active");
 
   /* Toggle between hiding and showing the active panel */
-  const panel = document.querySelector(".panel");
   if (panel.style.maxHeight) {
+    if (handler === "open") return;
     panel.style.maxHeight = null;
-    acc.querySelector("span").textContent =
-      "Тицяй, щоб побачити перелік послуг";
+    acc.querySelector("span").textContent = isMobile
+      ? "Перелік послуг"
+      : "Тицяй, щоб побачити перелік послуг";
     acc.querySelector("svg").style.transform = "rotate(0deg)";
   } else {
+    if (handler === "close") return;
     panel.style.maxHeight = panel.scrollHeight + "px";
-    window.scrollTo(0, panel.offsetTop);
-    acc.querySelector("span").textContent = "Тицяй, щоб сховати перелік послуг";
+    panel.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    acc.querySelector("span").textContent = isMobile
+      ? "Перелік послуг"
+      : "Тицяй, щоб сховати перелік послуг";
     acc.querySelector("svg").style.transform = "rotate(180deg)";
   }
+}
+
+categotiesBtns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    const isThisActive = this.classList.contains("active");
+
+    categotiesBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    let currentCategory = "";
+
+    if (isThisActive) {
+      this.classList.remove("active");
+      //remove focus
+      this.blur();
+      //remove touch focus
+
+      currentCategory = "";
+    } else {
+      this.classList.add("active");
+      currentCategory = this.dataset.category;
+      accordionHandler("open");
+      panel.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+    const listsParent = document.querySelector(".services__wrap");
+    servicesLists.forEach((list) => {
+      if (currentCategory === "") {
+        listsParent.style.removeProperty("justify-content");
+        list.removeAttribute("style");
+        list.classList.remove("visually-hidden");
+        return;
+      } else {
+        listsParent.style.justifyContent = "center";
+        list.style.alignItems = "center";
+      }
+      if (list.dataset.services === currentCategory) {
+        list.classList.remove("visually-hidden");
+      } else {
+        list.classList.add("visually-hidden");
+      }
+    });
+  });
 });
